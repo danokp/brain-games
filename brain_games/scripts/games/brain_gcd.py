e3b70d8e04fb01c8a8a169fd50f8dbc977ec14a2
Daniil <danokp@mail.ru>
2022-12-16 01:28:14 +0700

    Fix brain_gcd.py

--- brain_games/scripts/games/brain_gcd.py
+++ brain_games/scripts/games/brain_gcd.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-from brain_games.scripts.brain_games_functions import (welcome_user,
-                                                       play_game,
-                                                       congratulate_winner)
+from brain_games.scripts.brain_games_functions import play_game
 import random
 
 
@@ -14,21 +12,19 @@ def calc_gcd(num_1, num_2):
                 return num
 
 
+def give_question_and_answer_brain_gcd():
+    random_num_first = 1
+    random_num_last = 100
+    random_num_1 = random.randint(random_num_first, random_num_last)
+    random_num_2 = random.randint(random_num_first, random_num_last)
+    correct_answer = str(calc_gcd(random_num_1, random_num_2))
+    question = f'{random_num_1} {random_num_2}'
+    return question, correct_answer
+
+
 def main():
-    name = welcome_user()
-    print('Find the greatest common divisor of given numbers.')
-
-    question_num = 3
-    while question_num > 0:
-        random_num_first = 1
-        random_num_last = 100
-        random_num_1 = random.randint(random_num_first, random_num_last)
-        random_num_2 = random.randint(random_num_first, random_num_last)
-        correct_answer = str(calc_gcd(random_num_1, random_num_2))
-        question = f'{random_num_1} {random_num_2}'
-        question_num = play_game(name, question, correct_answer, question_num)
-
-    congratulate_winner(name, question_num)
+    task = 'Find the greatest common divisor of given numbers.'
+    play_game(give_question_and_answer_brain_gcd, task)
 
 
 if __name__ == '__main__':
