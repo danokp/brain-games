c216926edd39472359668a0c107363b73ee9db19
Daniil <danokp@mail.ru>
2022-12-16 01:27:23 +0700

    Fix brain_even.py

--- brain_games/scripts/games/brain_even.py
+++ brain_games/scripts/games/brain_even.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-from brain_games.scripts.brain_games_functions import (welcome_user,
-                                                       play_game,
-                                                       congratulate_winner)
+from brain_games.scripts.brain_games_functions import play_game
 import random
 
 
@@ -9,19 +7,17 @@ def is_even(num):
     return 'yes' if num % 2 == 0 else 'no'
 
 
+def give_question_and_answer_brain_even():
+    random_num_first = 0
+    random_num_last = 1_000
+    random_num = random.randint(random_num_first, random_num_last)
+    correct_answer = is_even(random_num)
+    return random_num, correct_answer
+
+
 def main():
-    name = welcome_user()
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-
-    question_num = 3
-    while question_num > 0:
-        random_num_first = 0
-        random_num_last = 1_000
-        random_num = random.randint(random_num_first, random_num_last)
-        correct_answer = is_even(random_num)
-        question_num = play_game(name, random_num, correct_answer, question_num)
-
-    congratulate_winner(name, question_num)
+    task = 'Answer "yes" if the number is even, otherwise answer "no".'
+    play_game(give_question_and_answer_brain_even, task)
 
 
 if __name__ == '__main__':
