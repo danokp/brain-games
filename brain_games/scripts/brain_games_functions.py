ba4023fee8e263565b02e2e1c198525ee194461f
Daniil <danokp@mail.ru>
2022-12-16 01:26:42 +0700

    Fix brain_games_functions.py

--- brain_games/scripts/brain_games_functions.py
+++ brain_games/scripts/brain_games_functions.py
@@ -9,7 +9,7 @@ def welcome_user():
     return name
 
 
-def play_game(name, question, correct_answer, question_num):
+def play_one_round(name, question, correct_answer, question_num):
     print(f'Question: {question}')
     answer = prompt.string('Your answer: ')
     if answer == correct_answer:
@@ -25,3 +25,16 @@ was '{correct_answer}'.\nLet's try again, {name}!")
 def congratulate_winner(name, question_num):
     if question_num == 0:
         print(f'Congratulations, {name}!')
+
+
+def play_game(game_name, task):
+    name = welcome_user()
+    print(task)
+    question_num = 3
+    while question_num > 0:
+        question, correct_answer = game_name()
+        question_num = play_one_round(name,
+                                      question,
+                                      correct_answer,
+                                      question_num)
+    congratulate_winner(name, question_num)
